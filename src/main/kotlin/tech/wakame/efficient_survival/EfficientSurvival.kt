e125696d33122d58e42e1d95d97b056b102350ee
wakame_tech <Kamata1919@gmail.com>
2021-07-15 14:47:44 +0900

    Initial commit

---
+++ src/main/kotlin/tech/wakame/efficient_survival/EfficientSurvival.kt
@@ -0,0 +1,13 @@
+package tech.wakame.efficient_survival
+
+import org.bukkit.plugin.java.JavaPlugin
+
+class EfficientSurvival : JavaPlugin() {
+    override fun onEnable() {
+        // Plugin startup logic
+    }
+
+    override fun onDisable() {
+        // Plugin shutdown logic
+    }
+}
